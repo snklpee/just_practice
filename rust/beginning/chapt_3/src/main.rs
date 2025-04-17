5d43fdccceab4e5ba3b48b32a23c51426f882049
sankalp <manesm2403@gmail.com>
2025-04-17 12:26:08 +0530

    another example of changing the type of a variable while shadowing

--- rust/beginning/chapt_3/src/main.rs
+++ rust/beginning/chapt_3/src/main.rs
@@ -1,18 +1,6 @@
 fn main() {
-    let x = 5;
-
-    let x = x + 1;
-
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
-
-    {
-        let x = "`x` is now a string";
-        println!("Is x really a string in the inner scope now ? \n
-        lets se whats become of x:\n {x}")
-    }
-
-    println!("The value of x is: {x}");
+    let spaces = "   ";
+    println!("The value of spaces is an empty space: {spaces}");
+    let spaces = spaces.len();
+    println!("The value of spaces is now a number: {spaces}");
 }
