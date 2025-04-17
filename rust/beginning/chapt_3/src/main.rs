52eced41e4bd4d97cb1db82daa621c628b42737c
sankalp <manesm2403@gmail.com>
2025-04-17 14:45:01 +0530

    simple math operations

--- rust/beginning/chapt_3/src/main.rs
+++ rust/beginning/chapt_3/src/main.rs
@@ -1,5 +1,27 @@
 fn main() {
-    let mut spaces = "   ";
-    spaces = spaces.len();
-    // this does not compile because rust does not allow the mutation of variable's type
+    // addition
+    let sum = 5 + 10;
+    println!("sum is {}",sum);
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+    println!("difference is {}",difference);
+
+    // multiplication
+    let product = 4 * 30;
+    println!("product is {}",product);
+
+    // division
+    let quotient = 56.7 / 32.2;
+    println!("quotient is {}",quotient);
+    let truncated = -15.1 / 3.0; // Results in -1
+    println!("truncated is {}",truncated);
+
+    // remainder
+    let remainder = 43 % 5;
+    println!("remainder is {}",remainder);
+
+    // powers and exponentiation
+    let exponent = 2.1f64;
+    println!("exponentiation is: {}",exponent.powf(2.0));
 }
