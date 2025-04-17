b2049ca40c9c9b8c40aa01bddd56251441a87254
sankalp <manesm2403@gmail.com>
2025-04-17 12:20:06 +0530

    shadowing also works with different data types.

--- rust/beginning/chapt_3/src/main.rs
+++ rust/beginning/chapt_3/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    {
+        let x = "`x` is now a string";
+        println!("Is x really a string in the inner scope now ? \n
+        lets se whats become of x:\n {x}")
+    }
+
+    println!("The value of x is: {x}");
 }
